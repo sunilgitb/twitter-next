2902436ab4ecc6587fe5e404d426c0f8169b7483
Sunil Besra <sunilbesra28@gmail.com>
2023-09-06 21:51:59 +0530

    changes done

--- firebase.js
+++ firebase.js
@@ -7,12 +7,14 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY, 
-  authDomain: "twitter-v4.firebaseapp.com",
-  projectId: "twitter-v4",
-  storageBucket: "twitter-v4.appspot.com",
-  messagingSenderId: "958780669916",
-  appId: "1:958780669916:web:c0ab24c3f605b0a233d919",
+  apiKey: "AIzaSyApmZB36B7laWKCSSQwg9dTUN3e-pxmP0I",
+  authDomain: "phoneotp-51be2.firebaseapp.com",
+  databaseURL: "https://phoneotp-51be2-default-rtdb.firebaseio.com",
+  projectId: "phoneotp-51be2",
+  storageBucket: "phoneotp-51be2.appspot.com",
+  messagingSenderId: "831921641811",
+  appId: "1:831921641811:web:bc9a7074435ff0a1df69a6",
+  measurementId: "G-2BZ5PNXJR5"
 };
 
 // Initialize Firebase
